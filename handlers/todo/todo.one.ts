e086eba494ca6af0d13a6578247d9b8218da81d3
muhammad abdurrahman <rachman.sd@gmail.com>
2022-12-12 00:34:55 +0700

    feat: Create handler to find One data

--- handlers/todo/todo.one.ts
+++ handlers/todo/todo.one.ts
@@ -1,3 +1,61 @@
-const getOneTodoItemHandler = () => async () => {
+import { PrismaClientKnownRequestError, PrismaClientUnknownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
+import { Static, Type } from "@sinclair/typebox";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
+const TodoItemReply = Type.Object({
+  status: Type.String(),
+  message: Type.String(),
+  data: Type.Object({
+    id: Type.Number(),
+    title: Type.String(),
+    activity_group_id: Type.Number(),
+    is_active: Type.String(),
+    priority: Type.String(),
+    created_at: Type.String(),
+    updated_at: Type.String(),
+    deleted_at: Type.Null({default: null})
+  })
+});
+
+const ErrorTodoItemReply = Type.Object({ status: Type.String(), message: Type.String(), data: Type.Object({}) });
+
+export const TodoItemSchema = {
+  schema: {
+    response: {
+      200: TodoItemReply,
+      400: ErrorTodoItemReply,
+      500: ErrorTodoItemReply
+    }
+  }
 }
+
+export type TodoItemReplyType = Static<typeof TodoItemReply> | Static<typeof ErrorTodoItemReply>;
+
+const getOneTodoItemHandler = (app: FastifyInstance) => async (req: FastifyRequest<{Params: {todo_id: string}}>, reply: FastifyReply) => {
+  try {
+    const {is_active, ...todo} = await app.prisma.todos.findFirstOrThrow({where: {id: +req.params.todo_id}});
+    reply.code(200)
+      .type("application/json; charset=utf-8")
+      .send({ status: "Success", message: "Success", data: {...todo, is_active: is_active ? "1": "0"} });
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      if (error.name === "NotFoundError") {
+        reply.code(404)
+        .type("application/json; charset=utf-8")
+        .send({ status: "Not Found", message: `Todo with ID ${req.params.todo_id} Not Found`, data: {} });
+        return;
+      }
+    }
+    if (error instanceof PrismaClientValidationError || error instanceof PrismaClientKnownRequestError || error instanceof PrismaClientUnknownRequestError) {
+      reply.code(500)
+        .type("application/json; charset=utf-8")
+        .send({ status: "Error", message: "Database error", data: [] });
+      return;
+    }
+    reply.code(500)
+        .type("application/json; charset=utf-8")
+        .send({ status: "Error", message: "Unknown Error", data: [] });
+  }
+}
+
+export default getOneTodoItemHandler;
