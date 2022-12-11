52a4520f8fa3bfaea0b552dcd2247a62d15416bb
muhammad abdurrahman <rachman.sd@gmail.com>
2022-12-11 12:36:50 +0700

    fix: fixed Insert activity Handler

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import Fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
-import insertActivityHandler, { InsertActivitySchema, InsertActivityType } from './handlers/activity/activity.insert';
+import insertActivityHandler, { InsertActivityBodyType, InsertActivitySchema, InsertActivityType } from './handlers/activity/activity.insert';
 
 export default function build (opt: FastifyServerOptions) {
   const app: FastifyInstance = Fastify(opt).withTypeProvider<TypeBoxTypeProvider>();
-  app.post<{Reply: InsertActivityType}>('/activity-groups', InsertActivitySchema, insertActivityHandler);
+  app.post<{Body: InsertActivityBodyType, Reply: InsertActivityType}>('/activity-groups',InsertActivitySchema, insertActivityHandler(app));
   return app;
 }
--- handlers/activity/activity.insert.ts
+++ handlers/activity/activity.insert.ts
@@ -1,36 +1,62 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError, PrismaClientUnknownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
 import { Static, Type } from "@sinclair/typebox"
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { isoDate } from "../../helper";
 
-export const InsertActivityReply = Type.Object({
+const InsertActivityReply = Type.Object({
   status: Type.String(),
   message: Type.String(),
   data: Type.Object({
-    created_at: Type.String({ format: "date" }),
-    updated_at: Type.String({ format: "date" }),
+    created_at: Type.String(),
+    updated_at: Type.String(),
     id: Type.Number(),
     title: Type.String(),
-    email: Type.String({ format: "email" })
+    email: Type.String({ format: "email" }),
   })
+});
+
+const ErrorActivityReply = Type.Object({ status: Type.String(), message: Type.String(), data: Type.Object({}) })
+
+const InsertBodyActivity = Type.Object({
+  title: Type.String(),
+  email: Type.String({ format: "email" })
 });
 
 export const InsertActivitySchema = {
   schema: {
+    body: InsertBodyActivity,
     response: {
-      200: InsertActivityReply
+      200: InsertActivityReply,
+      400: Type.Object({ status: Type.String(), message: Type.String(), data: Type.Object({}) }),
+      500: Type.Object({ status: Type.String(), message: Type.String(), data: Type.Object({}) })
     }
   }
 }
 
-export type InsertActivityType = Static<typeof InsertActivityReply>;
+export type InsertActivityType = Static<typeof InsertActivityReply> | Static<typeof ErrorActivityReply>;
+export type InsertActivityBodyType = Static<typeof InsertBodyActivity>;
 
+// type PrismaClientType = PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>
 
-export default function insertActivityHandler(_: FastifyRequest, reply: FastifyReply) {
-  reply.status(200).send({
-    status: "Success", message: "Success", data: {
-      created_at: (new Date()).toString(),
-      updated_at: (new Date()).toString(),
-      id: 1188,
-      title: "test"
+const insertActivityHandler = (app: FastifyInstance) => async (req: FastifyRequest<{ Body: InsertActivityBodyType }>, reply: FastifyReply) => {
+  try {
+    const activity = await app.prisma.activities.create({ data: { ...req.body, created_at: isoDate(), updated_at: isoDate() } });
+    reply.code(200)
+      .type("application/json; charset=utf-8")
+      .send({ status: "Success", message: "Success", data: activity });
+  } catch (error) {
+    if (error instanceof PrismaClientValidationError || error instanceof PrismaClientKnownRequestError || error instanceof PrismaClientUnknownRequestError) {
+      reply.code(500)
+        .type("application/json; charset=utf-8")
+        .send({ status: "Error", message: "Database error", data: {} });
+      return;
     }
-  });
+    console.log(error);
+    reply.code(500)
+        .type("application/json; charset=utf-8")
+        .send({ status: "Error", message: "Unknown Error", data: {} });
+  }
 }
+
+export default insertActivityHandler;
