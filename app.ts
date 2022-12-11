6951b356c0abc555b46432f769aa8928def92123
muhammad abdurrahman <rachman.sd@gmail.com>
2022-12-11 16:54:10 +0700

    fix: Update route handler

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import Fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
 import insertActivityHandler, { InsertActivityBodyType, InsertActivitySchema, InsertActivityType } from './handlers/activity/activity.insert';
 import listActivityHandler, { ActivityReplyListType, ActivitySchema } from './handlers/activity/activity.list';
+import getActivityHandler, { ActivityOneSchema, ActivityReplyType } from './handlers/activity/activity.one';
+import removeActivityHandler, {ActivityRemoveSchema, RemoveActivityReplyType } from './handlers/activity/activity.remove';
 
 export default function build (opt: FastifyServerOptions) {
   const app: FastifyInstance = Fastify(opt).withTypeProvider<TypeBoxTypeProvider>();
   app.post<{Body: InsertActivityBodyType, Reply: InsertActivityType}>('/activity-groups',InsertActivitySchema, insertActivityHandler(app));
   app.get<{Reply: ActivityReplyListType}>('/activity-groups', ActivitySchema, listActivityHandler(app));
-  app.get('/activity-groups/:group_id', () => {});
-  app.delete('/activity-groups/:group_id', () => {});
+  app.get<{Reply: ActivityReplyType, Params: {group_id: string}}>('/activity-groups/:group_id',ActivityOneSchema, getActivityHandler(app));
+  app.delete<{Reply: RemoveActivityReplyType, Params: {group_id: string}}>('/activity-groups/:group_id', ActivityRemoveSchema, removeActivityHandler(app));
   return app;
 }
