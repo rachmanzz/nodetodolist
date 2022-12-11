4261a27ec8c0fc7c984a0abba6ab0d2974eec290
muhammad abdurrahman <rachman.sd@gmail.com>
2022-12-11 12:46:27 +0700

    fix: Remove log info and set Error handler

--- server.ts
+++ server.ts
@@ -1,12 +1,34 @@
 import app from "./app";
+import prismaPlugin from "./plugins/prisma";
+import formbody from "@fastify/formbody";
+import Fastify from 'fastify';
 
 const server = app({
   logger: {
-    level: "info",
+    level: "error",
     transport: {
       target: 'pino-pretty'
     }
   }
+});
+
+server.register(formbody, { parser: str => JSON.parse(str) });
+server.register(prismaPlugin);
+server.setErrorHandler((error, req, reply) => {
+  if (error.validation && error.validation.length >= 1) {
+    const validation = error.validation[0];
+
+    const missing = Array.isArray(validation.params.missingProperty) && validation.params.missingProperty.join(", ") || validation.params.missingProperty;
+    reply.code(400)
+      .type("application/json; charset=utf-8")
+      .send({ status: "Bad Request", message: `${missing} cannot be null`, data: {} });
+    return;
+
+  }
+  // may need filter error type, but as documentation not spesific the error code, so I make it default error
+  reply.code(500)
+    .type("application/json; charset=utf-8")
+    .send({ status: error.name, message: error.message, data: {} });
 });
 
 server.listen({ port: 3000 }, (err) => {
