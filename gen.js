4b9433806b5a5ad226ee13d13f4163cfb1d67535
muhammad abdurrahman <rachman.sd@gmail.com>
2022-12-12 12:18:35 +0700

    fix: update gen resources

--- gen.js
+++ gen.js
@@ -10,8 +10,31 @@ generator client {
 
 datasource db {
   provider = "${process.env.DB_DRIVER}"
-  url      = ":memory:"
+  url      = "${process.env.DB_DRIVER === "mysql" ? `mysql://${process.env.MYSQL_USER|| 'root'}:${process.env.MYSQL_PASSWORD|| 'my-secret-pw'}@${process.env.MYSQL_HOST|| '127.0.0.1'}:${process.env.MYSQL_PORT || '3306'}/${process.env.MYSQL_DBNAME || 'todolist'}` : 'file:./dev.db'}"
 }
+
+model Activities {
+  id         Int     @id @default(autoincrement())
+  email      String
+  title      String
+  created_at String // string instead of DateTime because we need store Iso String of Date
+  updated_at String
+  deleted_at String?
+  todos      Todos[]
+}
+
+model Todos {
+  id                Int        @id @default(autoincrement())
+  title             String
+  is_active         Boolean
+  priority          String
+  created_at        String
+  updated_at        String
+  deleted_at        String?
+  activity          Activities @relation(fields: [activity_group_id], references: [id])
+  activity_group_id Int
+}
+
 `;
 
 fs.writeFile("./prisma/schema.prisma", datasource, (err) => {
